f51847e7b44687cd02edc603e60f2e2bef2cfe8c
obayedul islam <obaed.cse@gmail.com>
2022-11-06 03:47:17 +0600

    Secured Scccessful using JWT Token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const app = express();
@@ -17,11 +18,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'Unauthorized Access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if(err){
+            return res.status(403).send({message: 'Forbidden Access'})
+        }
+        req.decoded = decoded;
+        next();
+
+    })
+}
+
 async function run() {
     try{
         const serviceCollection = client.db('carHub').collection('services');
 
         const orderCollection = client.db('carHub').collection('orders');
+        
+        //JWT Token
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+            res.send({token})
+        })
 
         app.get('/services', async(req, res) => {
             const query = {};
@@ -39,7 +63,14 @@ async function run() {
         })
 
         //Orders API
-        app.get('/orders', async(req, res) => {
+        app.get('/orders', verifyJWT, async(req, res) => {
+            const decoded = req.decoded;
+            console.log('Inside orders API', decoded);
+
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: "Forbidden Access"})
+            }
+
             let query = {};
             if(req.query.email){
                 query = {
@@ -51,13 +82,13 @@ async function run() {
             res.send(orders)
         })
 
-        app.post('/orders', async(req, res) => {
+        app.post('/orders', verifyJWT, async(req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
-        app.patch('/orders/:id', async(req, res) => {
+        app.patch('/orders/:id', verifyJWT, async(req, res) => {
             const id = req.params.id;
             const status = req.body.status;
             const query = {_id: ObjectId(id)};
